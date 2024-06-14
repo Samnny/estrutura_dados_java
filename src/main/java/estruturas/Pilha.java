b3bd20fc4a490db0a288de72a2c39332dda836f5
Samnny <arolllysamay@gmail.com>
2024-06-14 19:01:11 -0300

    feat : Pilha(add, show)

--- src/main/java/estruturas/Pilha.java
+++ src/main/java/estruturas/Pilha.java
@@ -1,4 +1,26 @@
-package estruturas;
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pilha {
+    private List<String> pilha;
+    private int tamanho;
+
+    public Pilha(){
+        this.pilha = new ArrayList<>();
+        this.tamanho = 0;
+    }
+
+    public void adicionarInicio(String nomeJogo){
+        this.pilha.addFirst(nomeJogo);
+        this.tamanho++;
+    }
+
+    public void mostrarItens(){
+        for (String item : this.pilha){
+            System.out.println("Nome do jogo: "+item);
+        }
+    }
 }
